a8a5a2744f0f1378dd40f7b7fdde3f1810ea78e6
abhijeet-14 <abhijeet.rts@gmail.com>
2022-10-08 17:45:47 +0530

    add logger to an API

--- todo/todo_api/views.py
+++ todo/todo_api/views.py
@@ -1,18 +1,43 @@
 
+import uuid
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
+import logging
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 class AddTask(APIView):
 
     def post(self, request):
         """Add new task"""
-        print("Enter: Add New Task")
-        print(request.data)
-        result = {
-            "result": "Test_Result",
-            "isTrue": True
-        }
+        err_ref = uuid.uuid4()
+        try:
+            logger.info("Enter: Add New Task")
+            logger.info(request.data)
 
-        return Response(result, status=status.HTTP_200_OK)
+            logger.info("info logger")
+            logger.debug("debug logger")
+            logger.warn("warning logger")
+            logger.error("error logger")
+            logger.critical("critical logger")
+
+            val = 1/0
+
+            result = {
+                "result": "Test_Result",
+                "isTrue": True
+            }
+
+            logger.info("Exit: Add New Task")
+            return Response(result, status=status.HTTP_200_OK)
+        except Exception as err:
+            logger.error(f"Exit : Error occured : {str(err)} :: err_ref : {err_ref}")
+            return Response(
+                {
+                    "message": str(err), 
+                    "isTrue": False,
+                    "err_ref": err_ref
+                }, 
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
